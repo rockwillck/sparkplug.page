67543c1ae9913181143d4663ee1a3e50cae55bb4
rockwillck <38663064+rockwillck@users.noreply.github.com>
2024-02-27 20:54:00 -0600

    email

--- app.js
+++
@@ -1,7 +0,0 @@
-setInterval(() => {
-    if (window.scrollY > window.innerHeight * 0.3) {
-        document.getElementById("nav").style.boxShadow = "0 0 20px rgba(0, 0, 0, 0.5)"
-    } else {
-        document.getElementById("nav").style.boxShadow = ""
-    }
-})
