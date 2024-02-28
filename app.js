c49947cdaae210211149131f11c0b0ece76af1d4
rockwillck <38663064+rockwillck@users.noreply.github.com>
2024-02-27 20:50:43 -0600

    favicon and stuff

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-// Start coding here
+setInterval(() => {
+    if (window.scrollY > window.innerHeight * 0.3) {
+        document.getElementById("nav").style.boxShadow = "0 0 20px rgba(0, 0, 0, 0.5)"
+    } else {
+        document.getElementById("nav").style.boxShadow = ""
+    }
+})
